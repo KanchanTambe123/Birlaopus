a8868754ff0c1baaf7195cd683cac207f2e1ce75
Kanchan Tambe <Kanchan.Tambe@teknopoint.local>
2026-06-24 16:32:10 +0530

    Uat

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -30,7 +30,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 	    features = {"src/test/resources/features"},
 	    glue = {"stepdefinition"},
-	    tags = "@Scope1",
+	    tags = "@Scope2",
 	    plugin = {
 	        "pretty",
 	        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
